4b9b4cbfed7a8f3eddfd0681dbae877a9e566d17
Ragul-j-1 <Ragul-j-1>
2025-10-29 20:42:03 +0530

    2

---
+++ Ml mini project/Customer Segmentation.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv("Mall_Customers.csv")
+
+
+x = df[['Age', 'Annual Income (k$)', 'Spending Score (1-100)']]
+
+
+scaler = StandardScaler()
+x_scaled = scaler.fit_transform(x)
+
+
+model = KMeans(n_clusters=3, random_state=42)
+df['cluster'] = model.fit_predict(x_scaled)
+
+
+plt.scatter(df['Annual Income (k$)'], df['Spending Score (1-100)'], 
+            c=df['cluster'], cmap='viridis')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.title('Customer Segmentation using K-Means')
+plt.show()
